926b182d0fcbe95b40ef1f6c36a29de0b729403d
WilliamHaxhija <williamhaxhija@gmail.com>
2024-05-06 17:36:37 +0200

    feat: add contents

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Dischi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 
@@ -24,17 +25,16 @@
         </header>
 
         <main class="bg-dark">
-            <div class="container">
-                <div class="row">
-                    <div class="col-4 bg-dark-subtle">
+            <div class="container text-center">
+                <div class="row justify-content-center py-4">
+                    <div class="col-3 bg-black py-3 px-4 m-3 border border-black rounded-4" v-for="album, index in albums">
 
-                        <div class="card bg-dark-subtle">
-                            <img src="" class="card-img-top" alt="">
+                        <div class="card bg-black text-light">
+                            <img :src="album.poster" class="card-img-top" :alt="album.title">
                             <div class="card-body">
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk
-                                    of the card's content.</p>
-                                    {{message}}
+                                <h5>{{album.title}}</h5>
+                                <span>{{album.author}}</span>
+                                <h6>{{album.year}}</h6>
                             </div>
                         </div>
 
--- js/script.js
+++ js/script.js
@@ -3,10 +3,19 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-        message: 'ciao'
-    }
+        albums: []
+    };
   },
   methods: {
-
+    getContentFromApi() {
+        axios.get('server.php')
+        .then((response) => {
+            this.albums = response.data;
+            console.log(this.albums);
+        });
+    }
+  },
+  mounted () {
+    this.getContentFromApi()
   }
 }).mount('#app')
