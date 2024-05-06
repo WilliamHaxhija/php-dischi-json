a3425b89ab3009cf59d559c8d28b83e3487a5625
WilliamHaxhija <williamhaxhija@gmail.com>
2024-05-06 16:15:12 +0200

    feat: add vue

--- index.php
+++ index.php
@@ -5,39 +5,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi</title>
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 
 <body>
-    <header class="py-3 bg-black">
-        <div class="container">
-            <div class="w-25">
-                <img class="w-25"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
-                    alt="logo">
-            </div>
-        </div>
-    </header>
 
-    <main class="bg-dark">
-        <div class="container">
-            <div class="row">
-                <div class="col-4 bg-dark-subtle">
-
-                    <div class="card bg-dark-subtle">
-                        <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
-                        </div>
-                    </div>
-
+    <div id="app">
+        <header class="py-3 bg-black">
+            <div class="container">
+                <div class="w-25">
+                    <img class="w-25"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
+                        alt="logo">
                 </div>
             </div>
-        </div>
-    </main>
+        </header>
+
+        <main class="bg-dark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4 bg-dark-subtle">
+
+                        <div class="card bg-dark-subtle">
+                            <img src="" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk
+                                    of the card's content.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+
+    <script src="./js/script.js"></script>
 </body>
 
 </html>
